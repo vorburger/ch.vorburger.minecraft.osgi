e95429a0cec2d3a6e2608d89c83c33419c36f68a
Michael Vorburger <mike@vorburger.ch>
2017-08-18 03:29:26 +0200

    PlayerJoinListener should only installBundle if !project.exists
    Because if it does exist, then it has been installed before.
    
    And if it has been installed before, then BundleManagerPersistence will
    have recorded it in ./osgi/installedBundles - and if that was edited to
    remove a user project, then we should not keep adding it back!

--- ch.vorburger.minecraft.osgi.users/src/main/java/ch/vorburger/minecraft/osgi/users/PlayerJoinListener.java
+++ ch.vorburger.minecraft.osgi.users/src/main/java/ch/vorburger/minecraft/osgi/users/PlayerJoinListener.java
@@ -60,8 +60,8 @@ public class PlayerJoinListener implements EventListener<ClientConnectionEvent.J
         if (!userProject1.exists()) {
             userProject1.mkdirs();
             new ProjectWriter().writeProject(new SimpleProjectTemplate(), userProject1);
+            bundleManager.installBundle(player, userProject1);
         }
-        bundleManager.installBundle(player, userProject1);
     }
 
 }
